2635ae1e62f79136067111fbc376f9506ac623fe
marcofrr <aluno19437@ipt.pt>
2020-10-02 21:44:31 +0100

    First commit

---
+++ config/index.ts
@@ -0,0 +1,12 @@
+const env = process.env.NODE_ENV || 'dev';
+
+type Config = {
+  serverUrl: string;
+  serverPort: number;
+  serverDatabase: string;
+  jwtSecret: string;
+};
+
+const config: Config = require(`./${env}`).default;
+
+export default config;
